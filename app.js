f782b5fc88413fd73fc15eadd58e17637aa1a888
Kasidech Nacharoenwutthikun <kasidech.official@gmail.com>
2017-08-08 17:07:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const request = require("request")
 
 const app = express()
 
-var app_token = "EAAMbFL9876YBAF2jvbW8BKhzVNeMyHrqWX8W9XRsXq5rzsANWpNdkG8L11ytLz64HHVNmYPOZCW93zz678pZBtodY5pJfKnpgdiqbVIZAcQJwWTQApsve0pWMyZCCL4OrTvWagUH0UrIe9jNO4p6kmleiVifUne1ZBGNhWEk5gQZDZD"
+var app_token = "EAABjlvgpvDEBANzcNngL9mipKXU2Hi6bOxZB6tGwjtSpDnuNMM9ttmARHnefBw2ebssduv4EskiJurBBnApkHFOOh88xAtZCkn5naOvkMeg1R84TdOERH76S8v6VFRGMMmUvGh8NMZAy96JRCLxPTseKyaOQs04HiLEbxjgbQZDZD"
+var app_tokenB = "EAACZCB9ZCwGHgBAOOJpI0QHIuzLXlStvxsDsgFVGngr73QxrGRN5BalQvuyc4A8FJHrqpei2oZABzeEHZChk0tUXjkXtarvjGNjXQc8l5OI3MRI0XKQKPXAW1woRy54qxCbTO7GZBNzeZBJ9jFWqGKcgDjXAV4binfwdX0MZCBvmAZDZD"
+
 app.set('port', (process.env.PORT || 5000))
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -26,20 +28,15 @@ app.get('/a', function (req, res) {
 });
 
 app.post('/a', function (req, res) {
+    console.log("app A")
     var data = req.body;
-    console.log(data)
-    // Make sure this is a page subscription
     if (data.object === 'page') {
 
-        // Iterate over each entry - there may be multiple if batched
         data.entry.forEach(function (entry) {
             var pageID = entry.id;
             var timeOfEvent = entry.time;
 
-            // Iterate over each messaging event
             entry.messaging.forEach(function (event) {
-                console.log(event)
-                console.log(event.postback)
                 if (event.message) {
                     receivedMessage(event);
                 }
@@ -51,11 +48,78 @@ app.post('/a', function (req, res) {
             });
         });
 
-        // Assume all went well.
-        //
-        // You must send back a 200, within 20 seconds, to let us know
-        // you've successfully received the callback. Otherwise, the request
-        // will time out and we will keep trying to resend.
+        res.sendStatus(200);
+    }
+});
+
+app.get('/b', function (req, res) {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === 'yyy') {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
+    }
+});
+
+app.post('/b', function (req, res) {
+    console.log("app B")
+    var data = req.body;
+    if (data.object === 'page') {
+
+        data.entry.forEach(function (entry) {
+            var pageID = entry.id;
+            var timeOfEvent = entry.time;
+
+            entry.messaging.forEach(function (event) {
+                if (event.message) {
+                    receivedMessage(event);
+                }
+                else if (event.postback) {
+                    receivedPostback(event);
+                } else {
+                    console.log("Webhook received unknown event: ", event);
+                }
+            });
+        });
+
+        res.sendStatus(200);
+    }
+});
+
+app.get('/c', function (req, res) {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === 'zzz') {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
+    }
+});
+
+app.post('/c', function (req, res) {
+    console.log("app C")
+    var data = req.body;
+    if (data.object === 'page') {
+
+        data.entry.forEach(function (entry) {
+            var pageID = entry.id;
+            var timeOfEvent = entry.time;
+
+            entry.messaging.forEach(function (event) {
+                if (event.message) {
+                    receivedMessage(event);
+                }
+                else if (event.postback) {
+                    receivedPostback(event);
+                } else {
+                    console.log("Webhook received unknown event: ", event);
+                }
+            });
+        });
+
         res.sendStatus(200);
     }
 });
@@ -68,7 +132,6 @@ function receivedMessage(event) {
 
     console.log("Received message for user %d and page %d at %d with message:",
         senderID, recipientID, timeOfMessage);
-    console.log(JSON.stringify(message));
 
     var messageId = message.mid;
 
@@ -77,8 +140,6 @@ function receivedMessage(event) {
 
     if (messageText) {
 
-        // If we receive a text message, check to see if it matches a keyword
-        // and send back the example. Otherwise, just echo the text we received.
         switch (messageText) {
             case 'gen':
                 sendGenericMessage(senderID);
@@ -239,15 +300,11 @@ function receivedPostback(event) {
     var recipientID = event.recipient.id;
     var timeOfPostback = event.timestamp;
 
-    // The 'payload' param is a developer-defined field which is set in a postback 
-    // button for Structured Messages. 
     var payload = event.postback.payload;
 
     console.log("Received postback for user %d and page %d with payload '%s' " +
         "at %d", senderID, recipientID, payload, timeOfPostback);
 
-    // When a postback is called, we'll send a message back to the sender to 
-    // let them know it was successful
     sendTextMessage(senderID, "Postback called");
 }
 
